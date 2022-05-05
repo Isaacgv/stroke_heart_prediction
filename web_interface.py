edd2598ed9d89bfdf2be0fec6025f0eba53107be
Patrick Saade <patrick-saade@hotmail.com>
2022-05-05 04:05:12 +0200

    industrialisation + final notebook + requirements.txt

--- web_interface.py
+++ web_interface.py
@@ -2,16 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-gender_list=np.array(["Male","Female"])
-yes_no=np.array(["Yes","No"])
-work_type_lit=np.array(['Private', 'Self employed', 'Govt job', 'children', 'Never worked'])
-residence_type_list=np.array(['Urban', 'Rural'])
+
 
 st.title('Stroke Prediction')
 #gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,bmi,smoking_status,stroke
 
-form = st.form(key='my_form')
+
+
+
+gender_list = np.array(["Male", "Female"])
+yes_no = np.array(["Yes", "No"])
+work_type_lit = np.array(['Private', 'Self employed', 'Govt job', 'children', 'Never worked'])
+residence_type_list = np.array(['Urban', 'Rural'])
+st.sidebar.write("""## Enter your information""")
+form = st.sidebar.form(key='my_form')
 first_name = form.text_input(label='First Name')
+st.session_state.firstname=first_name
 last_name = form.text_input(label='Last Name')
 age = form.number_input(label='Age', min_value=0, step=1, max_value=150)
 gender = form.radio("Select gender", gender_list)
@@ -24,10 +30,7 @@ avg_glucose_level = form.number_input(label='Enter your Average Glucose Level', 
 bmi = form.number_input(label='Enter your body mass index (bmi)', min_value=0.0, step=0.1)
 smoking_status = form.radio("Do you smoke ?", yes_no)
 submit_button = form.form_submit_button(label='Submit')
+if submit_button:
+    st.write(first_name)
+    st.sidebar.success("information sent successfully ")
 
-
-#import requests
-#url = 'http://localhost:8501/'
-#myobj = {'somekey': 'somevalue'}
-#x = requests.post(url, data = myobj)
-#st.write(x.url)
