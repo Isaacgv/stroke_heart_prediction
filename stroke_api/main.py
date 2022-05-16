774dfe3e9b5c8b259c5aa9f1abe025579120d5ca
ibrahim sobh <isobh@outlook.com>
2022-05-16 19:12:21 +0200

    Edits to FastApi and Web Interface

--- stroke_api/main.py
+++ stroke_api/main.py
@@ -5,12 +5,13 @@ from fastapi import FastAPI, Request
 from numpy import int64
 import pandas as pd
 import random
-import test
+from stroke_prediction.inference import make_prediction
+from fastapi.responses import JSONResponse
 import json
 app = FastAPI()
 file = "../models/classifier.pickle"
 
-def make_predicition(details: dict):
+def make_one_predicition(details: json):
     df_columns=['id', 'gender', 'age', 'hypertension', 'heart_disease',
              'ever_married','work_type', 'Residence_type', 'avg_glucose_level', 
              'bmi','smoking_status']
@@ -28,10 +29,13 @@ def make_predicition(details: dict):
                  'avg_glucose_level': float,
                  'bmi': float,
                  'smoking_status':object})
+    details= json.loads(details)
     details["id"]=1
     for column_name in df_columns:
         prediciton_df.loc[0,column_name] = details[column_name]
-    return {"result": int(test.make_prediction(prediciton_df)[0])}
+
+    prediction=int(make_prediction(prediciton_df)[0])
+    return {"prediction": prediction}
 
 
 def make_predicition_document(details: dict):
@@ -52,18 +56,17 @@ def make_predicition_document(details: dict):
                  'avg_glucose_level': float,
                  'bmi': float,
                  'smoking_status':object})
-    df["result"] = test.make_prediction(df)
+    df["result"] = make_prediction(df)
     print(df)
     return json.dumps(df.to_dict('index'))
     
 
-
-@app.post("/")
+@app.get("/predcit")
 async def root(request: Request):
-    res = await request.json()#{"message": "Hello World"}
-    make_predicition(res)
-    print(make_predicition(res))
-    return make_predicition(res)
+    res = await request.json()
+    result= make_one_predicition(res)
+    return result
+
 
 @app.post("/document")
 async def root(request: Request):
--- web_interface.py
+++ web_interface.py
@@ -1,23 +1,53 @@
-
-import requests
-import streamlit as st
+from ftplib import error_temp
+from typing import OrderedDict
+from matplotlib.font_manager import json_dump
 import pandas as pd
 import numpy as np
+
 import json
+from fastapi.responses import JSONResponse
+import requests
+import streamlit as st
 
 # interact with FastAPI endpoint
-backend ="http://0.0.0.0:8005/"
+BACKEND ="http://0.0.0.0:8005/"
 
-def get_prediction(url,features):
-    req = requests.post(url, data=json.dumps(features))
-    st.write(req.content)
+def get_prediction(features):
+    url =BACKEND + "predcit"
+    response = requests.get(url, json=json.dumps(features))
+    if response.status_code ==200:
+        results=response.json()
+        prediction = results["prediction"]
+        return prediction
 
-def get_prediction_document(url, features):
-    req = requests.post(url+"document", data=features.to_json(orient ='index'))
-    return req.content
+
+def get_prediction_document(features):
+    response = requests.post(BACKEND+"document", data=features.to_json(orient ='index'))
+    return response.content
+
+
+
+# Singular Prediction Page Section
+st.title("Heart Stroke Prediction")
+
+# CSS Changes for Side Bar 
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 500px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 500px;
+        margin-left: -500px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 with st.sidebar.expander("Single Predictions"):
-    with st.form(key='my_form') as form :
+    with st.form(key='my_form' ,clear_on_submit=True):
         gender_list = np.array(["Male", "Female"])
         yes_no = np.array(["Yes", "No"])
         work_type_lit = np.array(['Private', 'Self employed', 'Govt job', 'children', 'Never worked'])
@@ -25,61 +55,57 @@ with st.sidebar.expander("Single Predictions"):
         first_name = st.text_input(label='First Name')
         last_name = st.text_input(label='Last Name')
         age = st.number_input(label='Age', min_value=0, step=1, max_value=150)
-        gender = st.radio("Select gender", gender_list)
+        gender = st.radio("Select your Gender", gender_list)
         hypertension = st.radio("Did you had Hypertension in the past ?", yes_no)
-        heart_disease = st.radio("Did you had a heart problem in the past ?", yes_no)
+        heart_disease = st.radio("Did you had Heart Problem in the past ?", yes_no)
         ever_married = st.radio("Have you ever been married ?", yes_no)
-        work_type = st.radio("What is your work type ?", work_type_lit)
-        residence_type = st.radio("What is your living environment?", residence_type_list)
+        work_type = st.radio("What is your Work Type ?", work_type_lit)
+        residence_type = st.radio("What is your Living Environment?", residence_type_list)
         avg_glucose_level = st.number_input(label='Enter your Average Glucose Level', min_value=0.0, step=0.1)
-        bmi = st.number_input(label='Enter your body mass index (bmi)', min_value=0.0, step=0.1)
-        smoking_status = st.radio("Do you smoke ?", ('smoked', 'Unknown'))
+        bmi = st.number_input(label='Enter your Body Mass Index (bmi)', min_value=0.0, step=0.,format="%.2f")
+        smoking_status = st.radio("Do you Smoke ?", ('smoked','formerly smoked','Unknown'))
         doctor_name = st.text_input(label='Dc. First Name')
         doctor_last_name = st.text_input(label='Dc. Last Name')
-        submit_button = st.form_submit_button(label='Submit')
+        submit_button = st.form_submit_button(label='Predict')
 
-        if submit_button :
-            # Create Json object row
-            #gender,age,hypertension,heart_disease,ever_married,work_type,
-            #Residence_type,avg_glucose_level,bmi,smoking_status,stroke
-            myform_json= {"first_name": first_name,
-                          "last_name": last_name,
-                          "age" : age,
-                          "gender": gender,
-                          "hypertension": hypertension,
-                          "heart_disease": heart_disease,
-                          "ever_married" : ever_married,
-                          "work_type": work_type,
-                          "Residence_type": residence_type,
-                          "avg_glucose_level": avg_glucose_level,
-                          "bmi": bmi,
-                          "smoking_status": smoking_status,
-                          "doctor_name":doctor_name,
-                          "doctor_last_name":doctor_last_name
-                          }
-            st.write(get_prediction(backend, myform_json))
+if submit_button :
+    myform_json= {  "first_name": first_name,
+                    "last_name": last_name,
+                    "age" : age,
+                    "gender": gender,
+                    "hypertension": hypertension,
+                    "heart_disease": heart_disease,
+                    "ever_married" : ever_married,
+                    "work_type": work_type,
+                    "Residence_type": residence_type,
+                    "avg_glucose_level": avg_glucose_level,
+                    "bmi": bmi,
+                    "smoking_status": smoking_status,
+                    "doctor_name":doctor_name,
+                    "doctor_last_name":doctor_last_name
+                    }
+    
+    prediciton = get_prediction(myform_json)
+    message = f"{first_name} {last_name} You are at risk of a stroke !" if prediciton == 1 else  "you are safe to slay another day :)"
+    message_color = 'red' if prediciton == 1 else  'green'
+    st.markdown(f"<h3 style='text-align: left;color:{message_color}'> {(message)} </h3>", unsafe_allow_html=True)
+  
 
+# File  Prediction Page Section
 
 with st.sidebar.expander("Upload File for Predictions"):
     with st.form(key="predictions"):
         uploaded_files = st.file_uploader("Choose a CSV file")
         if uploaded_files:
             st.write("filename:", uploaded_files.name)
+
         submit_button_m = st.form_submit_button("Submit")
 
-    if submit_button_m:
-        st.success("File sent")
-
-
+        if submit_button_m:
+            st.success("File sent")
 
 if submit_button_m :
-            # Create Json object row
-            #gender,age,hypertension,heart_disease,ever_married,work_type,
-            #Residence_type,avg_glucose_level,bmi,smoking_status,stroke
-
-            d = pd.read_csv(uploaded_files)
-            #d = d.to_dict('index')
-            result = json.loads(get_prediction_document(backend, d))
-            print(result)
-            result = pd.read_json(result, orient ='index') 
-            st.write(result)
+    data = pd.read_csv(uploaded_files)
+    result = json.loads(get_prediction_document(data))
+    results_table = pd.read_json(result, orient ='index') 
+    st.write(results_table)
