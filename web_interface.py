d0e18ee8854f512982aa48f4568564db190de08c
Isaacgv <isaacgonzalesv@gmail.com>
2022-05-17 18:31:53 +0200

    Revert previous merge

--- web_interface.py
+++ web_interface.py
@@ -1,120 +1,85 @@
-import pandas as pd
-import numpy as np
 
-import json
-from data_model.stroke_model import Patient
 import requests
 import streamlit as st
+import pandas as pd
+import numpy as np
+import json
 
 # interact with FastAPI endpoint
-BACKEND ="http://0.0.0.0:8005/"
+backend ="http://0.0.0.0:8005/"
 
-def get_prediction():
-    features= details_to_json()
-    url =BACKEND + "predcit"
-    response = requests.get(url, json=features)
-    if response.status_code ==200:
-        results=response.json()
-        prediction = results["prediction"]
-        return prediction
+def get_prediction(url,features):
+    req = requests.post(url, data=json.dumps(features))
+    st.write(req.content)
 
-def color_neg(val):
-    color = 'red' if type(val) == str and val=="Risk of Stroke" else 'green'
-    return 'color: %s' %color
-def check_colmuns(data):
-    model_columns= Patient.getColumns()
-    data.columns in model_columns
-
-def get_prediction_document(data :pd.DataFrame):
-    df_json= data.to_dict(orient='records')
-    url =BACKEND + "document"
-    response = requests.get(url, json=df_json)
-    if response.status_code == 200:
-        result = json.loads(response.content)
-        prediction_df = pd.read_json(result, orient ='index') 
-        data["predicition"]=prediction_df["predicition"]
-        styler=data.style.applymap(color_neg)
-        data["predicition"] = data["predicition"].apply(lambda x:"Risk of Stroke" if x == 1 else "Normal")
-        st.dataframe(data.style.applymap(color_neg,subset=['predicition']))
-
-def details_to_json():
-    myform_json= {  "id" :0,
-                    "firstname": first_name,
-                    "lastname": last_name,
-                    "gender": gender,
-                    "age" : age,
-                    "hypertension": 1 if hypertension=="yes" else 0,
-                    "heart_disease": 1 if heart_disease=="yes" else 0,
-                    "ever_married" : ever_married,
-                    "work_type": work_type,
-                    "Residence_type": residence_type,
-                    "avg_glucose_level": avg_glucose_level,
-                    "bmi": bmi,
-                    "smoking_status": smoking_status
-                    }
-    return myform_json
-
-
-# Singular Prediction Page Section
-st.title("Heart Stroke Prediction")
-
-# CSS Changes for Side Bar 
-st.markdown(
-    """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        width: 500px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        width: 500px;
-        margin-left: -500px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+def get_prediction_document(url, features):
+    req = requests.post(url+"document", data=features.to_json(orient ='index'))
+    return req.content
 
 with st.sidebar.expander("Single Predictions"):
-    with st.form(key='my_form' ,clear_on_submit=True):
+    with st.form(key='my_form') as form :
         gender_list = np.array(["Male", "Female"])
         yes_no = np.array(["Yes", "No"])
         work_type_lit = np.array(['Private', 'Self employed', 'Govt job', 'children', 'Never worked'])
         residence_type_list = np.array(['Urban', 'Rural'])
         first_name = st.text_input(label='First Name')
         last_name = st.text_input(label='Last Name')
-        gender = st.radio("Select your Gender", gender_list)
         age = st.number_input(label='Age', min_value=0, step=1, max_value=150)
+        gender = st.radio("Select gender", gender_list)
         hypertension = st.radio("Did you had Hypertension in the past ?", yes_no)
-        heart_disease = st.radio("Did you had Heart Problem in the past ?", yes_no)
+        heart_disease = st.radio("Did you had a heart problem in the past ?", yes_no)
         ever_married = st.radio("Have you ever been married ?", yes_no)
-        work_type = st.radio("What is your Work Type ?", work_type_lit)
-        residence_type = st.radio("What is your Living Environment?", residence_type_list)
+        work_type = st.radio("What is your work type ?", work_type_lit)
+        residence_type = st.radio("What is your living environment?", residence_type_list)
         avg_glucose_level = st.number_input(label='Enter your Average Glucose Level', min_value=0.0, step=0.1)
-        bmi = st.number_input(label='Enter your Body Mass Index (bmi)', min_value=0.0, step=0.,format="%.2f")
-        smoking_status = st.radio("Do you Smoke ?", ('smoked','formerly smoked','Unknown'))
+        bmi = st.number_input(label='Enter your body mass index (bmi)', min_value=0.0, step=0.1)
+        smoking_status = st.radio("Do you smoke ?", ('smoked', 'Unknown'))
         doctor_name = st.text_input(label='Dc. First Name')
         doctor_last_name = st.text_input(label='Dc. Last Name')
-        submit_button = st.form_submit_button(label='Predict')
+        submit_button = st.form_submit_button(label='Submit')
 
-if submit_button :
-    prediciton = get_prediction()
-    message = f"{first_name} {last_name} You are at risk of a stroke !" if prediciton == 1 else  "you are safe to slay another day :)"
-    message_color = 'red' if prediciton == 1 else  'green'
-    st.markdown(f"<h3 style='text-align: left;color:{message_color}'> {(message)} </h3>", unsafe_allow_html=True)
-  
-# File  Prediction Page Section
+        if submit_button :
+            # Create Json object row
+            #gender,age,hypertension,heart_disease,ever_married,work_type,
+            #Residence_type,avg_glucose_level,bmi,smoking_status,stroke
+            myform_json= {"first_name": first_name,
+                          "last_name": last_name,
+                          "age" : age,
+                          "gender": gender,
+                          "hypertension": hypertension,
+                          "heart_disease": heart_disease,
+                          "ever_married" : ever_married,
+                          "work_type": work_type,
+                          "Residence_type": residence_type,
+                          "avg_glucose_level": avg_glucose_level,
+                          "bmi": bmi,
+                          "smoking_status": smoking_status,
+                          "doctor_name":doctor_name,
+                          "doctor_last_name":doctor_last_name
+                          }
+            st.write(get_prediction(backend, myform_json))
+
 
 with st.sidebar.expander("Upload File for Predictions"):
-    with st.form(key="predictions",clear_on_submit=True):
+    with st.form(key="predictions"):
         uploaded_files = st.file_uploader("Choose a CSV file")
         if uploaded_files:
             st.write("filename:", uploaded_files.name)
         submit_button_m = st.form_submit_button("Submit")
-        if submit_button_m:
-            st.success("File sent")
+
+    if submit_button_m:
+        st.success("File sent")
+
+
 
 if submit_button_m :
-    data = pd.read_csv(uploaded_files)
-    get_prediction_document(data)
+            # Create Json object row
+            #gender,age,hypertension,heart_disease,ever_married,work_type,
+            #Residence_type,avg_glucose_level,bmi,smoking_status,stroke
 
-
+            d = pd.read_csv(uploaded_files)
+            #d = d.to_dict('index')
+            result = json.loads(get_prediction_document(backend, d))
+            print(result)
+            result = pd.read_json(result, orient ='index') 
+            st.write(result)
